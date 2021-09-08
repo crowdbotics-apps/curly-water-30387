3185e6c39bbfbf2248737abc1b7ef6d7ced91dcf
Crowdbotics <team@crowdbotics.com>
2021-09-08 07:40:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "curly_water_30387Identifier",
+  url: "https://curly_water_30387Identifier.botics.co",
+  api: "https://curly_water_30387Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
